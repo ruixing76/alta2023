7450aa97375f309a07481cd16b283e928174cc1e
andy.mackinlay <andy.mackinlay@cultureamp.com>
2019-06-09 17:19:17 +1000

    removed `isAfter` warning

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Helmet from "react-helmet";
-import isAfter from "date-fns/is_after";
 
 import Layout from "../components/Layout";
 import "../styles/home.scss";

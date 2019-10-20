db251b25a4bbdb0d9b98059fb83c8199d0dd9aa8
andy.mackinlay <andy.mackinlay@cultureamp.com>
2019-10-20 11:29:20 +1100

    updated DST logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
         name: "Defence Science and Technology",
         longName: "Defence Science and Technology",
         level: "Gold",
-        image: "dst.png",
+        image: "dod.png",
         link: "https://www.dst.defence.gov.au/"
       },
       { 
